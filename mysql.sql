0f307e2b7a0f71978de547ec5a2455491a5ee108
Alberto Pose <albertopose@gmail.com>
2013-12-26 19:32:45 -0300

    Removing user creation from scripts.

--- mysql.sql
+++ mysql.sql
@@ -4,9 +4,4 @@ CREATE TABLE users(
    password VARCHAR(255) NOT NULL,
    nickname VARCHAR(255),
    email_Verified BOOLEAN
-);
-
-INSERT INTO users (id, email, password, nickname) VALUES (1, 'michael.scott@dundermifflin.com', 'test123', 'mike');
-INSERT INTO users (id, email, password, nickname) VALUES (2, 'dwight.schrute@dundermifflin.com', 'test123', 'dwight');
-INSERT INTO users (id, email, password, nickname) VALUES (3, 'jim.halpert@dundermifflin.com', 'test123', 'jim');
-
+)
--- sqlserver.sql
+++ sqlserver.sql
@@ -5,7 +5,3 @@ CREATE TABLE users(
    Nickname VARCHAR(255),
    Email_Verified BIT DEFAULT 'FALSE'
 );
-
-INSERT INTO users (Email, Password, Nickname) VALUES ('michael.scott@dundermifflin.com', 'test123', 'mike');
-INSERT INTO users (Email, Password, Nickname) VALUES ('dwight.schrute@dundermifflin.com', 'test123', 'dwight');
-INSERT INTO users (Email, Password, Nickname) VALUES ('jim.halpert@dundermifflin.com', 'test123', 'jim');
