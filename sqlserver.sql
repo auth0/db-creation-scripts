302a8e48cbcd1b3223d4bf88c39c49429a556b53
Alberto Pose <albertopose@gmail.com>
2013-12-26 19:27:56 -0300

    Improving SQL creation script for SQLServer.

--- sqlserver.sql
+++ sqlserver.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users(
-   Id INT,
+   Id INT IDENTITY PRIMARY KEY,
    Email VARCHAR(255) NOT NULL,
    Password VARCHAR(255) NOT NULL,
    Nickname VARCHAR(255),
-   Email_Verified BOOLEAN
+   Email_Verified BIT DEFAULT 'FALSE'
 );
 
-INSERT INTO users (id, Email, Password, Nickname) VALUES (1, 'michael.scott@dundermifflin.com', 'test123', 'mike');
-INSERT INTO users (id, Email, Password, Nickname) VALUES (2, 'dwight.schrute@dundermifflin.com', 'test123', 'dwight');
-INSERT INTO users (id, Email, Password, Nickname) VALUES (3, 'jim.halpert@dundermifflin.com', 'test123', 'jim');
+INSERT INTO users (Email, Password, Nickname) VALUES ('michael.scott@dundermifflin.com', 'test123', 'mike');
+INSERT INTO users (Email, Password, Nickname) VALUES ('dwight.schrute@dundermifflin.com', 'test123', 'dwight');
+INSERT INTO users (Email, Password, Nickname) VALUES ('jim.halpert@dundermifflin.com', 'test123', 'jim');
