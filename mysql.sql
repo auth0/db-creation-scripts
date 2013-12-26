7b8a52f8a900e50e00811cf20943af718cbe2789
Alberto Pose <albertopose@gmail.com>
2013-12-26 20:18:38 -0300

    Adding auto increment id for MySQL and Postgres DB.

--- mysql.sql
+++ mysql.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
-   id INT,
+   id MEDIUMINT NOT NULL AUTO_INCREMENT,
    email VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
    nickname VARCHAR(255),
    email_Verified BOOLEAN
-)
+);
--- postgres.sql
+++ postgres.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users(
-   id INT,
+   id SERIAL,
    email VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
    nickname VARCHAR(255),
